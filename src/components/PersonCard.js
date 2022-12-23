8167f79bd203bc4a0783b593506a4be9bbae6016
MedinaZS <101607623+MedinaZS@users.noreply.github.com>
2022-12-22 22:25:03 -0300

    First Commit

---
+++ src/components/PersonCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const PersonCard = (props) => {
+
+    //Desestructuracion de las propiedades
+    const {firstName, lastName, age, hairColor} = props;
+
+    return (
+        <div>
+            <h2> {lastName}, {firstName} </h2>
+            <p>Age: {age} </p>
+            <p>Hair Color: {hairColor} </p>
+        </div>
+    )
+}
+
+export default PersonCard
+
